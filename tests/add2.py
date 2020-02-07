2123561a883da95d851185bc9085ea6ed7f9f13e
Desmond <des_e_mond@hotmail.com>
2020-02-07 11:09:39 -0500

    it's good

---
+++ tests/add2.py
@@ -0,0 +1,13 @@
+'''' A function for adding 2 '''
+
+def hello_fun(my_int: int) -> int:
+    '''Returns an integer my_int + 2
+
+    Arguments:
+        my_int (int) -- any integer
+
+    Returns:
+        int -- my_int + 2
+
+    '''
+    return my_int + 2
---
+++ tests/test_add2.py
@@ -0,0 +1,6 @@
+
+import add2
+
+def test_fun():
+    ''' Tests fun() '''
+    assert add2.hello_fun(2) == 4
